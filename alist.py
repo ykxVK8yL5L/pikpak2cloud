1f14dceae39ca856bf8af4ba7097648d486a1a67
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2023-04-24 15:48:14 +0800

    alist

--- alist.py
+++ alist.py
@@ -7,11 +7,21 @@ import requests
 parser = argparse.ArgumentParser(description='操作Alist')
 parser.add_argument("--act", help="挂行动作", default="")
 parser.add_argument("--url", help="下载链接", default="")
-parser.add_argument("--token", help="Aliyun的token", default="")
+parser.add_argument("--refresh_token", help="Aliyun的refresh_token", default="")
+parser.add_argument("--token", help="Alist的token", default="")
 parser.add_argument("--dir", help="加密目录", default="")
 parser.add_argument("--file", help="上传的文件名", default="")
 args = parser.parse_args()
 
+
+def login():
+  url = "http://alist:5344/api/auth/login?Password=admin&Username=admin"
+  payload={}
+  headers = {}
+  response = requests.request("POST", url, headers=headers, data=payload)
+  logininfo = json.loads(response.text);
+  token = logininfo['data']['token']
+  return token
 
 def download(downloadUrl:str):
   urlinfo = downloadUrl.split("##");
@@ -19,12 +29,6 @@ def download(downloadUrl:str):
   os.system(cmd)
 
 def upload(filename: str):
-  url = "http://alist:5344/api/auth/login?Password=admin&Username=admin"
-  payload={}
-  headers = {}
-  response = requests.request("POST", url, headers=headers, data=payload)
-  logininfo = json.loads(response.text);
-  token = logininfo['data']['token']
   cmd = "curl -T 'zouzou.mp4' 'http://alist:5344/api/fs/put  -X 'PUT' \
   -H 'Accept: application/json, text/plain, */*' \
   -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8' \
@@ -38,14 +42,32 @@ def upload(filename: str):
   --insecure"
   os.system(cmd)
   
-def mount_aliyun(refresh_token:str,root_id:str):
-  
-  
+def mount_aliyun(refresh_token:str,root_id:str,token:str):
+  cmd = """
+  curl 'http://alist:5344/api/admin/storage/create' \
+  -H 'Accept: application/json, text/plain, */*' \
+  -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8' \
+  -H 'Authorization: """+token+"""' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Type: application/json;charset=UTF-8' \
+  -H 'Origin: http://alist:5344' \
+  -H 'Referer: http://alist:5344/@manage/storages/create' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36' \
+  --data-raw '{"id":1,"mount_path":"/aliyun","order":0,"driver":"AliyundriveOpen","cache_expiration":30,"status":"work","addition":"{\"root_folder_id\":\""""+root_id+"""\",\"refresh_token\":\""""+refresh_token+"""\",\"order_by\":\"\",\"order_direction\":\"\",\"oauth_token_url\":\"https://api.nn.ci/alist/ali_open/token\",\"client_id\":\"\",\"client_secret\":\"\",\"remove_way\":\"delete\",\"internal_upload\":false,\"AccessToken\":\"\"}","remark":"","modified":"2023-04-24T06:37:11.054213327Z","disabled":false,"enable_sign":false,"order_by":"","order_direction":"","extract_folder":"","web_proxy":false,"webdav_policy":"302_redirect","down_proxy_url":""}' \
+  --compressed \
+  --insecure
+  """
+  os.system(cmd)
+
+
 if __name__ == '__main__':
   if args.act=='mount':
-    mount_aliyun(args.token,args.dir)
+    mount_aliyun(args.refresh_token,args.dir,args.token)
   elif args.act=='download':
-    download(args.url)
+    download(args.url,args.token)
+  elif args.act=='login':
+    token=login()
+    print(token)
   else:
-    upload(args.file)
+    upload(args.file,args.token)
 
