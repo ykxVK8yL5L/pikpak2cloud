fd1b70278707a6673fe259b3a20535b8c72b6da4
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2023-04-25 16:15:30 +0800

    Update alist.py

--- alist.py
+++ alist.py
@@ -1,4 +1,23 @@
+import sys
+import argparse
+from json
 import requests
 
-r  = requests.get('http://alist:5344/public/index.html')
-print(r.text)
+parser = argparse.ArgumentParser(description='操作Alist')
+parser.add_argument("--act", help="操作", default="")
+args = parser.parse_args()
+
+def login():
+  url = "http://alist:5244/api/auth/login?Password=admin&Username=admin"
+  payload = {}
+  headers = {}
+  response = requests.request("POST", url, headers=headers, data=payload)
+  result = json.loads(r.text)
+  return result['data']['token']
+
+if __name__ == '__main__':
+  if args.act=='login':
+    auth_token=login()
+    print(auth_token)
+  else:
+    quit()
