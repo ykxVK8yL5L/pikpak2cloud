958e1a66aa6365bb1011d922f25215d9cb3ea33f
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2023-04-26 08:58:36 +0800

    Update alist.py

--- alist.py
+++ alist.py
@@ -1,11 +1,13 @@
-import sys,os,argparse,json,requests,time
+import sys,os,argparse,json,requests,time,base64
 from urllib import parse
 
 parser = argparse.ArgumentParser(description='操作Alist')
-parser.add_argument("--file", help="上传文件名", default="")
+parser.add_argument("--storage_body", help="存储设置base64加密", default="")
+parser.add_argument("--path", help="上传本地路径", default="")
+parser.add_argument("--fileName", help="上传文件名", default="")
 args = parser.parse_args()
 
-alist_host="http://alist:5344"
+alist_host="http://192.168.1.168:10017"
 UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 
 # 获取Token
@@ -19,36 +21,38 @@ def login():
 
 # 新建存储
 def storage_create(token,body):
+    data=json.loads(body.decode("utf-8").replace('\n', '').replace(' ', ''))
     url = f'{alist_host}/api/admin/storage/create'
     storage_header = {
         'UserAgent': UserAgent,
         'Authorization': token
     }
     try:
-        return requests.post(url, json=body, headers=storage_header, timeout=60)
+        return requests.post(url, json=data, headers=storage_header, timeout=60)
     except Exception as e:
         return {'code': -1, 'message': e}
   
 # 上传文件
 def Upload(token,localPath, remotePath, fileName, password = ''):
-    upload_header = {
-        'UserAgent': UserAgent,
-        'Authorization': token,
-        'File-Path': parse.quote(f'{remotePath}/{fileName}'),
-        'Password': password,
-        'Content-Length': f'{os.path.getsize(localPath)}'
-    }
     try:
-        return json.loads(requests.put(f'{url}/fs/put', headers=upload_header, data=open(localPath, 'rb').read()).text)
+        upload_header = {
+            'UserAgent': UserAgent,
+            'Authorization': token,
+            'File-Path': parse.quote(f'{remotePath}/{fileName}'),
+            'Password': password,
+            'Content-Length': f'{os.path.getsize(localPath)}'
+        }
+        return json.loads(requests.put(f'{alist_host}/api/fs/put', headers=upload_header, data=open(localPath, 'rb').read()).text)
     except Exception as e:
         return {'code': -1, 'message': e}
    
-
-
 if __name__ == '__main__':
     auth_token=login()
-    storage_create(auth_token,args.storage_body)
-    time.sleep(2)
-    Upload(auth_token,"downloads","encrypt_folder",args.file)
-    
-    
+    storage_result = json.loads(storage_create(auth_token,base64.b64decode(args.storage_body)).text)
+    if storage_result['message']=='success':
+        time.sleep(2)
+        upload_result=Upload(auth_token,args.path,"encrypt_folder",args.fileName)
+        print(upload_result)
+    else:
+        print("出错了，可能是存储字符串错误")
+        quit()
